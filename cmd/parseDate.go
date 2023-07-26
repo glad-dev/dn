8ccec7424f105510eeab2dd00a358747b4a6e23e
Gabriel Loyola Daiqui <gabriel@loyola.dev>
2023-07-26 15:33:48 +0200

    Fixes issue with date parsing

--- cmd/parseDate.go
+++ cmd/parseDate.go
@@ -11,12 +11,14 @@ import (
 func parseDate(dateStr string) (string, error) {
 	dateStr = strings.ReplaceAll(dateStr, ".", "-")
 
+	// Is date YYYY-MM-DD?
 	date, err := time.Parse("2006-01-02", dateStr)
 	if err == nil {
 		return date.Format(constants.DateFormat), nil
 	}
 
-	date, err = time.Parse("02.01.2006", dateStr)
+	// Is date DD-MM-YYYY?
+	date, err = time.Parse("02-01-2006", dateStr)
 	if err == nil {
 		return date.Format(constants.DateFormat), nil
 	}
